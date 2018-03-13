d47e2c54294e138e50bf1b3085b11ec9080ced0d
Arnaud <arnaud.dufour@utt.fr>
2018-03-13 23:37:32 +0100

    added Control-Allow-Origin and -Header

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ var c = new Client({
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 var port = process.env.PORT || 8080;        // set our port
