45f9efa678eaa8a3f9cebfa9ae6b0cabf3bcef5b
ValentinPeltier <valentinpeltier@yahoo.fr>
2019-02-25 11:21:16 +0100

    Add image model

--- src/api/models/index.js
+++ src/api/models/index.js
@@ -1,9 +1,11 @@
 module.exports = function(sequelize) {
   const Partner = sequelize.import(`${__dirname}/partner`)
   const Event = sequelize.import(`${__dirname}/event`)
+  const Image = sequelize.import(`${__dirname}/image`)
 
   return {
     Partner,
-    Event
+    Event,
+    Image
   }
 }
