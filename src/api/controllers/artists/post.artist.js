316a45598970440ef9808c0e57a02839d711a9e2
Arnaud Dufour <arnaudd@bam.tech>
2020-03-09 22:43:48 +0100

    some fix

--- src/api/controllers/artists/post.artist.js
+++ src/api/controllers/artists/post.artist.js
@@ -1,71 +1,70 @@
-const errorHandler = require('../../utils/errorHandler')
-const { check } = require('express-validator/check')
-const validateBody = require('../../middlewares/validateBody')
-const log = require('../../utils/log')(module)
-const isAuth = require('../../middlewares/isAuth')
-const isAdmin = require('../../middlewares/isAdmin')
-const path = require('path')
-const fs = require('fs')
+const errorHandler = require("../../utils/errorHandler");
+const { check } = require("express-validator/check");
+const validateBody = require("../../middlewares/validateBody");
+const log = require("../../utils/log")(module);
+const isAuth = require("../../middlewares/isAuth");
+const isAdmin = require("../../middlewares/isAdmin");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = app => {
-  app.post('/artists', [
-    check('name')
+  app.post("/artists", [
+    check("name")
       .isString()
       .exists(),
-    check('image')
+    check("image")
       .exists()
       .isString(),
-    check('links')
+    check("links")
       .exists()
       .isString(),
-    check('description')
+    check("description")
       .exists()
       .isString(),
-    check('eventDate')
+    check("eventDate")
       .optional()
       .isString(),
-    check('eventPlace')
+    check("eventPlace")
       .optional()
       .isString(),
-    check('visible')
+    check("visible")
       .optional()
       .isBoolean(),
     validateBody()
-  ])
-  app.post('/artists', [isAuth('artists-create'), isAdmin('artists-create')])
-  app.post('/artists', async (req, res) => {
-    const { Artist, Link } = app.locals.models
+  ]);
+  app.post("/artists", [isAuth("artists-create"), isAdmin("artists-create")]);
+  app.post("/artists", async (req, res) => {
+    const { Artist, Link } = app.locals.models;
     try {
-      const files = fs.readdirSync(path.join(__dirname, '../../../../temp'))
-      let file = files.find(f => f.indexOf(req.body.image) !== -1)
-      const oldfile = path.join(__dirname, '../../../../temp', file)
-      const newfile = path.join(__dirname, '../../../../images', file)
-      fs.copyFileSync(oldfile, newfile)
-      fs.unlinkSync(oldfile)
+      const files = fs.readdirSync(path.join(__dirname, "../../../../temp"));
+      let file = files.find(f => f.indexOf(req.body.image) !== -1);
+      const oldfile = path.join(__dirname, "../../../../temp", file);
+      const newfile = path.join(__dirname, "../../../../images", file);
+      fs.copyFileSync(oldfile, newfile);
+      fs.unlinkSync(oldfile);
 
-      const artists = await Artist.findAll({attributes: ['id']})
+      const artists = await Artist.findAll({ attributes: ["id"] });
 
       let artist = await Artist.create({
         ...req.body,
-        image: '/images/' + file,
+        image: "/images/" + file,
         index: artists.length
-      })
-      const links = JSON.parse(req.body.links)
-      await Promise.all(links.map(async link => {
-        await Link.create({
-          type: link.type,
-          uri: link.uri,
-          ArtistId: artist.id,
+      });
+      const links = JSON.parse(req.body.links);
+      await Promise.all(
+        links.map(async link => {
+          await Link.create({
+            type: link.type,
+            uri: link.uri,
+            ArtistId: artist.id
+          });
         })
-      }))
-      
-      log.info(`Artist ${artist.name} created`)
-      
-      return res
-        .status(200)
-        .json(artist)
+      );
+
+      log.info(`Artist ${artist.name} created`);
+      return res.status(200).json(artist);
     } catch (err) {
-      errorHandler(err, res)
+      errorHandler(err, res);
     }
-  })
-}
+  });
+};
